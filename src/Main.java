16b8c1efbb1249a645294cbdc36aa62a6bbbc4e6
kkt9253 <kkt9253@naver.com>
2025-11-16 14:12:26 +0900

    배열 돌리기 1(백준-골5): 나만의 풀이
    - 반시계로 돌아가는 공식을 찾아 2차원 배열 상에서 돌아가도록 구현

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,70 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.StringTokenizer;
+
 class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n, m;
-    static int[] d = new int[104];
-    // 초, 움직임 횟수, 현재 위치
-    static int[][][] dp = new int[104][34][2];
 
-    static int min = (int)-1e9;
+    static int n, m, r;
+    static int[][] arr = new int[304][304];
 
     public static void main(String[] args) throws IOException {
 
-        String[] line = br.readLine().split(" ");
-        n = Integer.parseInt(line[0]);
-        m = Integer.parseInt(line[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
         for (int i = 0; i < n; i++) {
-            d[i] = Integer.parseInt(br.readLine());
-        }
-        for (int i = 0; i < 104; i++) {
-            for (int j = 0; j < 34; j++) {
-                Arrays.fill(dp[i][j], -1);
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        bw.write(String.valueOf(Math.max(solve(0, m, 0), solve(0, m-1, 1))));
+        for (int i = 0; i < r; i++) {
+            solve();
+        }
 
-        br.close();
-        bw.flush();
-        bw.close();
+        for (int k = 0; k < n; k++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[k][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    static int solve(int second, int cnt, int position) {
-        if (cnt < 0) {
-            return min;
-        }
-        if (second == n) {
-            return 0;
-        }
+    static void solve() {
+        int[][] temp = new int[n+4][m+4];
+        for (int i = 0; i < Math.min(n, m) / 2; i++) {
 
-        if (dp[second][cnt][position] != -1) {
-            return dp[second][cnt][position];
-        }
+            // down
+            for (int j = i; j < n-i-1; j++) {
+                temp[j+1][i] = arr[j][i];
+            }
 
-        return dp[second][cnt][position] = Math.max(
-                solve(second+1, cnt, position),
-                solve(second+1, cnt-1, position^1)
-        ) + (d[second]-1 == position ? 1 : 0);
+            // right
+            for (int j = i; j < m - i - 1; j++) {
+                temp[n-i-1][j+1] = arr[n-i-1][j];
+            }
+
+            // left
+            for (int j = n-i-1; j > i; j--) {
+                temp[j-1][m-i-1] = arr[j][m-i-1];
+            }
+
+            // up
+            for (int j = m - i - 1; j > i; j--) {
+                temp[i][j - 1] = arr[i][j];
+            }
+        }
+        // copy
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = temp[i][j];
+            }
+        }
     }
 }
