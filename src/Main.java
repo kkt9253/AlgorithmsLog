998dd1b5397a656896fbc4cfbff8b1bbdebb5063
kkt9253 <kkt9253@naver.com>
2025-11-16 19:14:40 +0900

    빗물(백준-골5): Stack을 사용한 시뮬레이션
    - w 끝까지 left블록을 기준으로 기준보다 현재 블록이 작다면 스택에 저장, 같거나 크다면 스택에 저장된 블록을 꺼내 result += left - stk.pop한 후 left기준을 현재 블록으로 교체
    - 스택에 남아있는 블록도 처리하기 위해 right기준과 stk.pop을 비교하여 빗물이 담긴다면 result에 추가

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main {
@@ -11,86 +12,43 @@ class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
 
-    static int n, m, r;
-    static int[][] arr = new int[304][304];
+    static int h, w;
+    static int result;
 
     public static void main(String[] args) throws IOException {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int left = Integer.parseInt(st.nextToken());
+        Stack<Integer> stk = new Stack<>();
+        for (int i = 1; i < w; i++) {
+            int curH = Integer.parseInt(st.nextToken());
+            if (left > curH) {
+                stk.push(curH);
+            } else {
+                while (!stk.isEmpty()) {
+                    result += left - stk.pop();
+                }
+                left = curH;
+            }
+        }
+        int right = 0;
+        if (!stk.isEmpty()) {
+            right = stk.pop();
+        }
+        while (!stk.isEmpty()) {
+            int curH = stk.pop();
+            if (curH < right) {
+                result += Math.min(left, right) - curH;
+            } else if (curH > right) {
+                right = curH;
             }
         }
 
-        AllLayerRotation();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                sb.append(arr[i][j]).append(" ");
-            }
-            sb.append("\n");
-        }
-        bw.write(sb.toString());
+        bw.write(result + "\n");
         bw.flush();
-    }
-
-    static void AllLayerRotation() {
-        for (int i = 0; i < Math.min(n, m) / 2; i++) {
-            layerRotation(i);
-        }
-    }
-
-    static void layerRotation(int layer) {
-        int top = layer;
-        int bottom = n - 1 - layer;
-        int left = layer;
-        int right = m - 1 - layer;
-        int layerLen = 2*((bottom - top+1) + (right - left+1)) - 4;
-        int[] line = new int[layerLen];
-
-        // 1차원에 2차원 넣기
-        // left col
-        int idx = 0;
-        for (int i = top; i < bottom; i++) {
-            line[idx++] = arr[i][left];
-        }
-        // bottom row
-        for (int i = left; i < right; i++) {
-            line[idx++] = arr[bottom][i];
-        }
-        // right col
-        for (int i = bottom; i > top; i--) {
-            line[idx++] = arr[i][right];
-        }
-        // top row
-        for (int i = right; i > left; i--) {
-            line[idx++] = arr[top][i];
-        }
-
-        // 1차원 line에서 반시계로 당겨서 해당 인덱스의 값을 arr에 넣어주기
-        idx = 0;
-        int rot = r%layerLen;
-        // left col
-        for (int i = top; i < bottom; i++) {
-            arr[i][left] = line[(idx++ - rot + layerLen) % layerLen];
-        }
-        // bottom row
-        for (int i = left; i < right; i++) {
-            arr[bottom][i] = line[(idx++ - rot + layerLen) % layerLen];
-        }
-        // right col
-        for (int i = bottom; i > top; i--) {
-            arr[i][right] = line[(idx++ - rot + layerLen) % layerLen];
-        }
-        // top row
-        for (int i = right; i > left; i--) {
-            arr[top][i] = line[(idx++ - rot + layerLen) % layerLen];
-        }
     }
 }
