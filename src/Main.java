3707286daf0debf113590b18e9f100c34c544d15
kkt9253 <kkt9253@naver.com>
2025-11-16 20:34:03 +0900

    빗물(백준-골5): GPT 코드 리뷰 후 수정본
    - 각 블록에서 물이 고이려면 양쪽 벽이 존재해야 한다.
    - 고인 물의 깊이는 양쪽 벽 중 작은 벽이 결정한다.
    - 따라서 0 ~ w-1까지의 위치에서의 양쪽 최대 벽을 구하면 간단히 구현할 수 있는 아이디어

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main {
@@ -21,31 +20,29 @@ class Main {
         h = Integer.parseInt(st.nextToken());
         w = Integer.parseInt(st.nextToken());
 
+        int[] height = new int[w];
         st = new StringTokenizer(br.readLine());
-        int left = Integer.parseInt(st.nextToken());
-        Stack<Integer> stk = new Stack<>();
+        for (int i = 0; i < w; i++) {
+            height[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // left 최대 벽
+        int[] left = new int[w];
+        left[0] = height[0];
         for (int i = 1; i < w; i++) {
-            int curH = Integer.parseInt(st.nextToken());
-            if (left > curH) {
-                stk.push(curH);
-            } else {
-                while (!stk.isEmpty()) {
-                    result += left - stk.pop();
-                }
-                left = curH;
-            }
+            left[i] = Math.max(height[i], left[i-1]);
         }
-        int right = 0;
-        if (!stk.isEmpty()) {
-            right = stk.pop();
+
+        // right 최대 벽
+        int[] right = new int[w];
+        right[w - 1] = height[w - 1];
+        for (int i = w-2; i >= 0; i--) {
+            right[i] = Math.max(right[i+1], height[i]);
         }
-        while (!stk.isEmpty()) {
-            int curH = stk.pop();
-            if (curH < right) {
-                result += Math.min(left, right) - curH;
-            } else if (curH > right) {
-                right = curH;
-            }
+
+        // left & right 벽 중 작은거로부터 가둬진 현재 블록의 빗물 깊이
+        for (int i = 0; i < w; i++) {
+            result += Math.min(left[i], right[i]) - height[i];
         }
 
         bw.write(result + "\n");
