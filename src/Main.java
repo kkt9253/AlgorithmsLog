765bfc728ad113dc1c0eef89eb83db136860c9ca
kkt9253 <kkt9253@naver.com>
2025-11-29 15:00:32 +0900

    큰돌 교수님의 과제는 너무 어려워(큰돌): 계수정렬을 통한 시간복잡도 낮은 코드
    - 문제에선 0~100까지 0.01단위 범위의 점수가 주어지기에 0~10000로 점수를 정수로 만들고(87.03->8703) 5개 내림차순으로 출력

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,40 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 class Main {
 
-    static class Thing implements Comparable<Thing> {
-        int m, v;
-        Thing(int s, int e) {
-            this.m = s;
-            this.v = e;
-        }
-
-        @Override
-        public int compareTo(Thing o) {
-            return Integer.compare(this.v, o.v);
-        }
-    }
-
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws Exception {
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        ArrayList<Thing> things = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[10004];
+        int min = 10004;
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            things.add(new Thing(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            int a = (int) Math.round((Double.parseDouble(br.readLine())) * 100);
+            if (min > a) min = a;
+            arr[a]++;
         }
-        things.sort(Comparator.naturalOrder());
-
-        ArrayList<Integer> bags = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            bags.add(Integer.parseInt(br.readLine()));
-        }
-        bags.sort(Comparator.naturalOrder());
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int result = 0;
-        int idx = 0;
-        for (int i = 0; i < bags.size(); i++) {
-            while (idx < n && things.get(idx).m <= bags.get(i)) {
-                pq.offer(things.get(idx).v);
-                idx++;
-            }
-            if (!pq.isEmpty()) {
-                result += pq.poll();
+        int cnt = 0;
+        for (int i = min; cnt < 5; i++) {
+            while (arr[i] != 0) {
+                cnt++;
+                arr[i]--;
+                System.out.println(i / 100 + "." + (i % 100 < 10 ? "0"+ i % 100 : i % 100 / 10));
+                if (cnt >= 5) break;
             }
         }
-
-        bw.write(result + "\n");
-        bw.close();
     }
 }
 
+/*
+8
+95.6
+74.3
+88.2
+53.1
+92.7
+67.9
+88.2
+45.5
+ */
